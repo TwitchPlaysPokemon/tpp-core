0c18321e218824932fb7a564e442f455a9a8b6bc
Marcus Huderle <huderlem@gmail.com>
2017-08-18 19:29:55 -0700

    Add string formatting params to ITPPLogger api, and use invariant culture formatting

--- TPPCommon/Logging/TPPDebugLogger.cs
+++ TPPCommon/Logging/TPPDebugLogger.cs
@@ -5,9 +5,16 @@ using TPPCommon.PubSub.Events;
 
 namespace TPPCommon.Logging
 {
-    public class TPPDebugLogger : ITPPLogger
+    /// <summary>
+    /// Class which TPP services directly use to perform logging.
+    /// This class isn't responsible for perform the actual logging, rather it publishes logging
+    /// events, to which a logging service can listen.
+    /// 
+    /// This is meant to be used during local development, because it writes logs directly to the console, in addition to
+    /// publishing log events.
+    /// </summary>
+    public class TPPDebugLogger : TPPLoggerBase, ITPPLogger
     {
-        private string Prefix = string.Empty;
         private IPublisher Publisher;
 
         public TPPDebugLogger(IPublisher publisher)
@@ -48,36 +55,40 @@ namespace TPPCommon.Logging
         /// Log a debug event.
         /// </summary>
         /// <param name="message">log message</param>
-        public void LogDebug(string message)
+        /// <param name="args">string format args</param>
+        public void LogDebug(string message, params object[] args)
         {
-            this.PublishLog(new LogDebugEvent(ApplyPrefix(message)), "DEBUG");
+            this.PublishLog(new LogDebugEvent(this.NormalizeMessage(message, args)), "DEBUG");
         }
 
         /// <summary>
         /// Log a general information event.
         /// </summary>
         /// <param name="message">log message</param>
-        public void LogInfo(string message)
+        /// <param name="args">string format args</param>
+        public void LogInfo(string message, params object[] args)
         {
-            this.PublishLog(new LogInfoEvent(ApplyPrefix(message)), "INFO ");
+            this.PublishLog(new LogInfoEvent(this.NormalizeMessage(message, args)), "INFO ");
         }
 
         /// <summary>
         /// Log a warning event.
         /// </summary>
         /// <param name="message">log message</param>
-        public void LogWarning(string message)
+        /// <param name="args">string format args</param>
+        public void LogWarning(string message, params object[] args)
         {
-            this.PublishLog(new LogWarningEvent(ApplyPrefix(message)), "WARN ");
+            this.PublishLog(new LogWarningEvent(this.NormalizeMessage(message, args)), "WARN ");
         }
 
         /// <summary>
         /// Log an error event.
         /// </summary>
         /// <param name="message">log message</param>
-        public void LogError(string message)
+        /// <param name="args">string format args</param>
+        public void LogError(string message, params object[] args)
         {
-            this.PublishLog(new LogErrorEvent(ApplyPrefix(message)), "ERROR");
+            this.PublishLog(new LogErrorEvent(this.NormalizeMessage(message, args)), "ERROR");
         }
 
         /// <summary>
@@ -85,18 +96,20 @@ namespace TPPCommon.Logging
         /// </summary>
         /// <param name="message">log message</param>
         /// <param name="e">exception</param>
-        public void LogError(string message, Exception e)
+        /// <param name="args">string format args</param>
+        public void LogError(string message, Exception e, params object[] args)
         {
-            this.PublishLog(new LogErrorExceptionEvent(ApplyPrefix(message), e?.Message, e?.StackTrace), "ERROR");
+            this.PublishLog(new LogErrorExceptionEvent(this.NormalizeMessage(message, args), e?.Message, e?.StackTrace), "ERROR");
         }
 
         /// <summary>
         /// Log a critical error event.
         /// </summary>
         /// <param name="message">log message</param>
-        public void LogCritical(string message)
+        /// <param name="args">string format args</param>
+        public void LogCritical(string message, params object[] args)
         {
-            this.PublishLog(new LogCriticalEvent(ApplyPrefix(message)), "FATAL");
+            this.PublishLog(new LogCriticalEvent(this.NormalizeMessage(message, args)), "FATAL");
         }
 
         /// <summary>
@@ -104,9 +117,10 @@ namespace TPPCommon.Logging
         /// </summary>
         /// <param name="message">log message</param>
         /// <param name="e">exception</param>
-        public void LogCritical(string message, Exception e)
+        /// <param name="args">string format args</param>
+        public void LogCritical(string message, Exception e, params object[] args)
         {
-            this.PublishLog(new LogCriticalExceptionEvent(ApplyPrefix(message), e?.Message, e?.StackTrace), "FATAL");
+            this.PublishLog(new LogCriticalExceptionEvent(this.NormalizeMessage(message, args), e?.Message, e?.StackTrace), "FATAL");
         }
     }
 }
