f54428b752dcf462f212a8d3f9da3d225c3b6ed9
Felk <de.felix.koenig@gmail.com>
2020-03-22 19:34:39 +0100

    config WIP

---
+++ Core/Configuration/IrcConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Core.Configuration
+{
+	// properties need setters for deserialization
+	[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local")]
+	public class IrcConfig
+	{
+		/* connection information */
+		public string Channel { get; private set; } = "felkbot";
+
+		/* account information */
+		public string Username { get; private set; } = "justinfan27365461784";
+		public string Password { get; private set; } = "oauth:mysecret";
+
+		/* communication settings */
+		public enum SuppressionType { Whisper, Message, Command }
+		public HashSet<SuppressionType> Suppressions { get; private set; }
+			= ((SuppressionType[]) Enum.GetValues(typeof(SuppressionType))).ToHashSet(); // all values by default
+		// list of usernames and channels that may receive outbound messages even with suppression enabled
+		public HashSet<string> SuppressionOverrides { get; private set; } = new HashSet<string>();
+
+	}
+}
---
+++ Core/Configuration/RootConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Core.Configuration
+{
+	// properties need setters for deserialization
+	[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local")]
+	public class RootConfig
+	{
+		public IrcConfig Irc { get; private set; } = new IrcConfig();
+
+		public int StartingPokeyen { get; private set; } = 100;
+		public int StartingTokens { get; private set; } = 0;
+
+	}
+}
