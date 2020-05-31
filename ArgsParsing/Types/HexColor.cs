e0a047d720f562546d4be6fca784a37e74555e9b
Felk <de.felix.koenig@gmail.com>
2020-05-31 14:04:04 +0200

    add hex color parser

---
+++ ArgsParsing/Types/HexColor.cs
@@ -0,0 +1,18 @@
+﻿namespace ArgsParsing.Types
+{
+    /// <summary>
+    /// Wrapper around a color in hexadecimal string notation, e.g. <c>FF0000</c> for pure red.
+    /// The string is _not_ prefixed with a hash symbol.
+    /// </summary>
+    public class HexColor
+    {
+        public string HexColorString { get; }
+
+        public HexColor(string hexColorString)
+        {
+            HexColorString = hexColorString;
+        }
+
+        public static implicit operator string(HexColor hexColor) => hexColor.HexColorString;
+    }
+}
