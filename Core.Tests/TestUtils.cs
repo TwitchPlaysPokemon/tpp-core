6e5c2d193b7c802cc64fff678abecf751cdb0a11
Felk <de.felix.koenig@gmail.com>
2020-06-25 18:58:46 +0200

    use Instant instead of DateTime in TestUtils, formatting

--- Core.Tests/TestUtils.cs
+++ Core.Tests/TestUtils.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using NodaTime;
 using Persistence.Models;
 
 namespace Core.Tests
 {
     public static class TestUtils
     {
-        public static User MockUser(string name) => new User(Guid.NewGuid().ToString(),
-            name, name, name.ToLower(), null, DateTime.UnixEpoch, DateTime.UnixEpoch, null, 0, 0);
+        public static User MockUser(string name) => new User(
+            id: Guid.NewGuid().ToString(),
+            name: name, twitchDisplayName: name, simpleName: name.ToLower(), color: null,
+            firstActiveAt: Instant.FromUnixTimeSeconds(0),
+            lastActiveAt: Instant.FromUnixTimeSeconds(0),
+            lastMessageAt: null,
+            pokeyen: 0, tokens: 0);
     }
 }
