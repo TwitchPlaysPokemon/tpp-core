5b90fdd592aa757fdec2e6db8b6b7fdd9a04b190
Felix König <de.felix.koenig@gmail.com>
2020-07-08 16:36:12 +0200

    hook up some static commands (#62)
    * hook up some static commands
    
    * add cooldowns to easter egg commands
    
    * remove gauntlet command
    
    * format code
    
    * remove obsolete TODOs
    
    Co-authored-by: GitHub Action <action@github.com>

--- Core/Message.cs
+++ Core/Message.cs
@@ -2,15 +2,23 @@ using Persistence.Models;
 
 namespace Core
 {
+    public enum MessageSource
+    {
+        Chat,
+        Whisper,
+    }
+
     public class Message
     {
         public User User { get; }
         public string MessageText { get; }
+        public MessageSource MessageSource { get; }
 
-        public Message(User user, string messageText)
+        public Message(User user, string messageText, MessageSource messageSource)
         {
             User = user;
             MessageText = messageText;
+            MessageSource = messageSource;
         }
     }
 }
--- Core/Tpp.cs
+++ Core/Tpp.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading.Tasks;
+using ArgsParsing;
+using Core.Commands;
 using Core.Configuration;
 using Microsoft.Extensions.Logging;
+using NodaTime;
+using Persistence.Models;
 
 namespace Core
 {
@@ -12,17 +18,50 @@ namespace Core
     {
         private readonly ILogger _logger;
         private readonly RootConfig _rootConfig;
+        private readonly CommandProcessor _commandProcessor;
 
         public Tpp(ILoggerFactory loggerFactory, RootConfig rootConfig)
         {
             _logger = loggerFactory.CreateLogger<Tpp>();
             _rootConfig = rootConfig;
+            ArgsParser argsParser = Setups.SetUpArgsParser();
+            _commandProcessor = Setups.SetUpCommandProcessor(loggerFactory, argsParser);
+        }
+
+        private async Task OnMessage(Message message)
+        {
+            _logger.LogDebug($"< {message.User}: {message.MessageText}");
+            string[] parts = message.MessageText.Split(" ");
+            string? firstPart = parts.FirstOrDefault();
+            string? commandName = firstPart switch
+            {
+                var name when message.MessageSource == MessageSource.Whisper => name,
+                var name when message.MessageSource == MessageSource.Chat
+                              && name.StartsWith("!") => name.Substring(startIndex: 1),
+                _ => null
+            };
+            if (commandName != null)
+            {
+                CommandResult result = await _commandProcessor
+                    .Process(commandName, parts.Skip(1).ToImmutableList(), message);
+                if (result.Response != null)
+                {
+                    _logger.LogDebug($"> {message.User}: {result.Response}");
+                }
+            }
         }
 
         public async Task Run()
         {
             _logger.LogInformation("Hi!");
             await Task.Delay(TimeSpan.FromMilliseconds(100));
+
+            // not actually hooked up yet, mock an incoming message
+            var user = new User("userid", "user", "user", "user", color: null,
+                Instant.FromUnixTimeSeconds(0), Instant.FromUnixTimeSeconds(0), null, 0, 0);
+            await OnMessage(new Message(user, "!thanks", MessageSource.Chat));
+            await OnMessage(new Message(user, "thanks", MessageSource.Whisper));
+
             // TODO do all the things
             _logger.LogInformation("Bye!");
         }
