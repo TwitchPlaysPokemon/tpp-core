aa8035e5f3f38bc1420747a8aba574db0128084f
Felk <de.felix.koenig@gmail.com>
2020-07-10 19:03:25 +0200

    improve naming in NonvolatileStatus struct

--- Common/PkmnModels/Status.cs
+++ Common/PkmnModels/Status.cs
@@ -5,12 +5,12 @@ namespace Common.PkmnModels
     [DataContract]
     public struct NonvolatileStatus
     {
-        [DataMember(Name = "brn")] public bool Brn { get; set; }
-        [DataMember(Name = "frz")] public bool Frz { get; set; }
-        [DataMember(Name = "par")] public bool Par { get; set; }
-        [DataMember(Name = "psn")] public bool Psn { get; set; }
-        [DataMember(Name = "slp")] public int Slp { get; set; }
-        [DataMember(Name = "tox")] public int Tox { get; set; }
+        [DataMember(Name = "brn")] public bool Burn { get; set; }
+        [DataMember(Name = "frz")] public bool Freeze { get; set; }
+        [DataMember(Name = "par")] public bool Paralysis { get; set; }
+        [DataMember(Name = "psn")] public bool Poison { get; set; }
+        [DataMember(Name = "tox")] public int BadPoison { get; set; }
+        [DataMember(Name = "slp")] public int Sleep { get; set; }
     }
 
     [DataContract]
