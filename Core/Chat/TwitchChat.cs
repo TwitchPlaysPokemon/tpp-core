2387fce0c122e4285c9b134cbc097c0100870112
Felk <de.felix.koenig@gmail.com>
2020-07-17 20:30:48 +0200

    make the bot use '/me' and fix message length limits

--- Core/Chat/TwitchChat.cs
+++ Core/Chat/TwitchChat.cs
@@ -19,7 +19,10 @@ namespace Core.Chat
     public sealed class TwitchChat : IChat
     {
         public event EventHandler<MessageEventArgs> IncomingMessage = null!;
-        private static readonly MessageSplitter MessageSplitter = new MessageSplitter(maxMessageLength: 500);
+        private static readonly MessageSplitter MessageSplitterRegular = new MessageSplitter(
+            maxMessageLength: 500 - "/me ".Length);
+        private static readonly MessageSplitter MessageSplitterWhisper = new MessageSplitter(
+            maxMessageLength: 500 - "/w ,,,,,''''',,,,,''''',,,,, ".Length);
 
         private readonly ILogger<TwitchChat> _logger;
         private readonly IClock _clock;
@@ -71,9 +74,9 @@ namespace Core.Chat
             _logger.LogDebug($">#{_ircChannel}: {message}");
             await Task.Run(() =>
             {
-                foreach (string part in MessageSplitter.FitToMaxLength(message))
+                foreach (string part in MessageSplitterRegular.FitToMaxLength(message))
                 {
-                    _twitchClient.SendMessage(_ircChannel, part);
+                    _twitchClient.SendMessage(_ircChannel, "/me " + part);
                 }
             });
         }
@@ -89,7 +92,7 @@ namespace Core.Chat
             _logger.LogDebug($">@{target.SimpleName}: {message}");
             await Task.Run(() =>
             {
-                foreach (string part in MessageSplitter.FitToMaxLength(message))
+                foreach (string part in MessageSplitterWhisper.FitToMaxLength(message))
                 {
                     _twitchClient.SendWhisper(target.SimpleName, part);
                 }
