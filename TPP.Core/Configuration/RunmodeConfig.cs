0866c70faaad767481c5d74a20f5ab950484a483
Felk <de.felix.koenig@gmail.com>
2021-01-07 19:12:35 +0100

    add InputConfig to RunmodeConfig

--- TPP.Core/Configuration/RunmodeConfig.cs
+++ TPP.Core/Configuration/RunmodeConfig.cs
@@ -3,5 +3,7 @@ namespace TPP.Core.Configuration
     public class RunmodeConfig : ConfigBase, IRootConfig
     {
         public string Schema => "./config.runmode.schema.json";
+
+        public InputConfig InputConfig { get; init; } = new();
     }
 }
