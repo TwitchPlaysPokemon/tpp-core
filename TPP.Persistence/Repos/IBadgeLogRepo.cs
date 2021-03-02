2000fd40f60976648978ea80c27c5f5d1608a96b
felk <de.felix.koenig@gmail.com>
2021-03-02 20:35:43 +0100

    add badge log, add TransferBadge() to badge repo

---
+++ TPP.Persistence/Repos/IBadgeLogRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NodaTime;
+using TPP.Persistence.Models;
+
+namespace TPP.Persistence.Repos
+{
+    public static class BadgeLogType
+    {
+        public const string TransferGift = "gift";
+
+        // collect all the types being used here instead of scattering string literals across the codebase
+    }
+
+    public interface IBadgeLogRepo
+    {
+        public Task<BadgeLog> Log(
+            string badgeId,
+            string badgeLogType,
+            string? userId,
+            Instant timestamp,
+            IDictionary<string, object?>? additionalData = null);
+    }
+}
