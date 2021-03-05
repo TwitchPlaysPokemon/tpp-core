74c2aeda96d95f7dabd1926308509d2e6f0706cf
felk <de.felix.koenig@gmail.com>
2021-03-05 18:34:17 +0100

    add badge stats calculation

--- TPP.Persistence.MongoDB/Repos/BadgeRepo.cs
+++ TPP.Persistence.MongoDB/Repos/BadgeRepo.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.IdGenerators;
 using MongoDB.Driver;
@@ -16,16 +17,35 @@ using TPP.Persistence.MongoDB.Serializers;
 
 namespace TPP.Persistence.MongoDB.Repos
 {
-    public class BadgeRepo : IBadgeRepo
+    public class BadgeRepo : IBadgeRepo, IBadgeStatsRepo
     {
         private const string CollectionName = "badges";
+        private const string CollectionNameStats = "badgestats";
 
         public readonly IMongoCollection<Badge> Collection;
+        public readonly IMongoCollection<BadgeStat> CollectionStats;
         private readonly IMongoBadgeLogRepo _badgeLogRepo;
         private readonly IClock _clock;
 
+        /// Instant at which some update was deployed that is expected to majorly disrupt badge rarities,
+        /// e.g. if a whole new generation was added to pinball.
+        private readonly Instant _lastRarityUpdate;
+
+        /// Minimum amount of time that must pass before a badge that was created before the last rarity update
+        /// gets ignored for rarity calculations. This is necessary to have a smooth rarity transition period.
+        private readonly Duration _rarityCalculationTransition;
+
+        private readonly Instant _whenGen2WasAddedToPinball = Instant.FromUtc(2018, 3, 28, 12, 00);
+
+        /// Ratio at which badges from all sources are included in the rarity calculation,
+        /// as opposed to only badges from "natural" generation sources (pinball).
+        /// Because the rarity value is used to determine transmutation results, but transmuting directly changes
+        /// how many of a species' badges exist, this is a relatively small percentage to dampen the feedback loop.
+        private const double CountExistingFactor = 0.2;
+
         static BadgeRepo()
         {
+            Debug.Assert(CountExistingFactor >= 0 && CountExistingFactor <= 1, "factor must be a ratio");
             BsonClassMap.RegisterClassMap<Badge>(cm =>
             {
                 cm.MapIdProperty(b => b.Id)
@@ -40,14 +60,32 @@ namespace TPP.Persistence.MongoDB.Repos
                 cm.MapProperty(b => b.SellingSince).SetElementName("selling_since")
                     .SetIgnoreIfNull(true);
             });
+            BsonClassMap.RegisterClassMap<BadgeStat>(cm =>
+            {
+                cm.MapIdProperty(b => b.Species)
+                    .SetSerializer(PkmnSpeciesSerializer.Instance);
+                cm.MapProperty(b => b.Count).SetElementName("count");
+                cm.MapProperty(b => b.CountGenerated).SetElementName("count_generated");
+                cm.MapProperty(b => b.RarityCount).SetElementName("rarity_count");
+                cm.MapProperty(b => b.RarityCountGenerated).SetElementName("rarity_count_generated");
+                cm.MapProperty(b => b.Rarity).SetElementName("rarity");
+            });
         }
 
-        public BadgeRepo(IMongoDatabase database, IMongoBadgeLogRepo badgeLogRepo, IClock clock)
+        public BadgeRepo(
+            IMongoDatabase database,
+            IMongoBadgeLogRepo badgeLogRepo,
+            IClock clock,
+            Instant? lastRarityUpdate = null,
+            Duration? rarityCalculationTransition = null)
         {
             database.CreateCollectionIfNotExists(CollectionName).Wait();
             Collection = database.GetCollection<Badge>(CollectionName);
+            CollectionStats = database.GetCollection<BadgeStat>(CollectionNameStats);
             _badgeLogRepo = badgeLogRepo;
             _clock = clock;
+            _lastRarityUpdate = lastRarityUpdate ?? _whenGen2WasAddedToPinball;
+            _rarityCalculationTransition = rarityCalculationTransition ?? Duration.FromDays(90);
             InitIndexes();
         }
 
@@ -147,6 +185,7 @@ namespace TPP.Persistence.MongoDB.Repos
                     return (object?)null;
                 });
             }
+            await RenewBadgeStats(onlyTheseSpecies: badges.Select(b => b.Species).ToImmutableHashSet());
 
             foreach (var tpl in badges.Select(b => (b.UserId, b.Species)).Distinct())
             {
@@ -156,5 +195,84 @@ namespace TPP.Persistence.MongoDB.Repos
             }
             return updatedBadges.ToImmutableList();
         }
+
+        public async Task RenewBadgeStats(IImmutableSet<PkmnSpecies>? onlyTheseSpecies = null)
+        {
+            Instant now = _clock.GetCurrentInstant();
+            Instant startTime = Instant.Min(_lastRarityUpdate, now - _rarityCalculationTransition);
+
+            IAggregateFluent<Badge> pipeline = Collection.Aggregate();
+            if (onlyTheseSpecies != null)
+                pipeline = pipeline.Match(stat => onlyTheseSpecies.Contains(stat.Species));
+            // TODO workaround for https://jira.mongodb.org/browse/CSHARP-3449
+            // Using LINQ causes the badge source enum value to be implicitly converted to its ordinal value,
+            // which causes the comparisons against the "source" field to never match and give wrong results.
+            // Remove this JSON blob and uncomment the code below once there is a solution for that.
+            List<BsonDocument> stats = await pipeline.Group(@"
+            {
+                ""_id"": ""$species"",
+                ""count"": {""$sum"": {
+                    ""$cond"": [{""$ne"": [""$user"", null]}, 1, 0]
+                }},
+                ""count_generated"": {""$sum"": {
+                    ""$cond"": [{""$eq"": [""$source"", ""pinball""]}, 1, 0]
+                }},
+                ""rarity_count"": {""$sum"": {
+                    ""$cond"": [{""$and"": [{""$ne"": [""$user"", null]},
+                                            {""$gte"": [""$created_at"", ISODate(""" + startTime + @""")]}]}, 1, 0]
+                }},
+                ""rarity_count_generated"": {""$sum"": {
+                    ""$cond"": [{""$and"": [{""$eq"": [""$source"", ""pinball""]},
+                                            {""$gte"": [""$created_at"", ISODate(""" + startTime + @""")]}]}, 1, 0]
+                }}
+            }").ToListAsync();
+            // var stats = await pipeline
+            //     .Group(b => b.Species, group => new
+            //     {
+            //         Species = group.Key,
+            //         Count = group.Count(b => b.UserId != null),
+            //         CountGenerated = group.Count(b => b.Source == Badge.BadgeSource.ManualCreation),
+            //         RarityCount = group.Count(b => b.UserId != null && b.CreatedAt >= startTime),
+            //         RarityCountGenerated =
+            //             group.Count(b => b.Source == Badge.BadgeSource.ManualDistribution && b.CreatedAt >= startTime),
+            //     })
+            //     .SortBy(stat => stat.Species)
+            //     .ToListAsync();
+
+            long totalGenerated = await Collection.CountDocumentsAsync(b =>
+                b.Source == Badge.BadgeSource.Pinball && b.CreatedAt >= startTime);
+            long totalExisting = await Collection.CountDocumentsAsync(b =>
+                b.UserId != null && b.CreatedAt >= startTime);
+
+            if (stats.Count == 0) return;
+            await CollectionStats.BulkWriteAsync(stats.Select(stat =>
+                {
+                    double rarityGenerated = stat["rarity_count_generated"].AsInt32 /*stat.RarityCountGenerated*/ /
+                                             (double)totalGenerated;
+                    double rarityExisting = stat["rarity_count"].AsInt32 /*stat.RarityCount*/ / (double)totalExisting;
+                    double rarity = rarityGenerated * (1 - CountExistingFactor) + rarityExisting * CountExistingFactor;
+                    BadgeStat statEntity = new(
+                        Species: PkmnSpecies.OfId(stat["_id"].AsString) /*stat.Species*/,
+                        Count: stat["count"].AsInt32 /*stat.Count*/,
+                        CountGenerated: stat["count_generated"].AsInt32 /*stat.CountGenerated*/,
+                        RarityCount: stat["rarity_count"].AsInt32 /*stat.RarityCount*/,
+                        RarityCountGenerated: stat["rarity_count_generated"].AsInt32 /*stat.RarityCountGenerated*/,
+                        Rarity: rarity);
+                    return new ReplaceOneModel<BadgeStat>(
+                        Builders<BadgeStat>.Filter.Where(b =>
+                            b.Species == PkmnSpecies.OfId(stat["_id"].AsString) /*stat.Species*/),
+                        statEntity)
+                    {
+                        IsUpsert = true
+                    };
+                }),
+                new BulkWriteOptions { IsOrdered = false });
+        }
+
+        public async Task<ImmutableSortedDictionary<PkmnSpecies, BadgeStat>> GetBadgeStats() =>
+            (await CollectionStats
+                .Find(FilterDefinition<BadgeStat>.Empty)
+                .ToListAsync())
+            .ToImmutableSortedDictionary(stat => stat.Species, stat => stat);
     }
 }
