1fce07ae386088cc7dd16284376e453bd50a93dd
felk <de.felix.koenig@gmail.com>
2021-05-27 20:34:55 +0200

    fix some warnings

--- TPP.Core/Commands/CommandExtensions.cs
+++ TPP.Core/Commands/CommandExtensions.cs
@@ -23,11 +23,8 @@ namespace TPP.Core.Commands
             ctx.ArgsParser.Parse<T1, T2, T3, T4, T5>(ctx.Args);
 
         /// Replace the command execution with a different one.
-        public static Command WithExecution(this Command command, Command.Execute newExecution)
-        {
-            return new Command(command.Name, newExecution)
-            { Aliases = command.Aliases, Description = command.Description };
-        }
+        public static Command WithExecution(this Command command, Command.Execute newExecution) =>
+            new(command.Name, newExecution) { Aliases = command.Aliases, Description = command.Description };
 
         /// Replace the command execution with one that only executes the original execution
         /// if a condition is met, and returns some ersatz result otherwise.
@@ -47,7 +44,7 @@ namespace TPP.Core.Commands
         public static Command WithGlobalCooldown(this Command command, Duration duration)
         {
             var cooldown = new GlobalCooldown(SystemClock.Instance, duration);
-            return command.WithCondition(ctx => cooldown.CheckLapsedThenReset());
+            return command.WithCondition(_ => cooldown.CheckLapsedThenReset());
         }
 
         /// Replace the command execution with one that does nothing
