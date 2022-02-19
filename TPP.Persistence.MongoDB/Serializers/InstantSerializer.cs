92ace4b3cca1e547b6540e1f50e9163eacd85f15
felk <de.felix.koenig@gmail.com>
2022-02-19 14:09:14 +0100

    mongodb: use new linq3 driver

--- TPP.Persistence.MongoDB/Serializers/InstantSerializer.cs
+++ TPP.Persistence.MongoDB/Serializers/InstantSerializer.cs
@@ -28,29 +28,4 @@ namespace TPP.Persistence.MongoDB.Serializers
             context.Writer.WriteDateTime(value.ToUnixTimeMilliseconds());
         }
     }
-
-    // TODO workaround for https://jira.mongodb.org/browse/CSHARP-3449
-    // remove this class once that bug is fixed
-    public class InstantBsonTypeMapper : ICustomBsonTypeMapper
-    {
-        public static readonly InstantBsonTypeMapper Instance = new();
-
-        private InstantBsonTypeMapper()
-        {
-        }
-
-        public bool TryMapToBsonValue(object value, out BsonValue bsonValue)
-        {
-            if (value is Instant instant)
-            {
-                bsonValue = new BsonDateTime(instant.ToDateTimeUtc());
-                return true;
-            }
-            else
-            {
-                bsonValue = default!;
-                return false;
-            }
-        }
-    }
 }
