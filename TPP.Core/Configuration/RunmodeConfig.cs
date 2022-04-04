8620a4973dc359c79bf38b21af7de3047b3a18a1
felk <de.felix.koenig@gmail.com>
2022-04-04 20:03:57 +0200

    improve config: include some descriptions in json schema

--- TPP.Core/Configuration/RunmodeConfig.cs
+++ TPP.Core/Configuration/RunmodeConfig.cs
@@ -1,18 +1,25 @@
+using System.ComponentModel;
+
 namespace TPP.Core.Configuration
 {
     public class RunmodeConfig : ConfigBase, IRootConfig
     {
         public string Schema => "./config.runmode.schema.json";
 
+        [Description("Host of the HTTP input server where inputs can be polled from.")]
         public string InputServerHost { get; init; } = "127.0.0.1";
+        [Description("Port of the HTTP input server where inputs can be polled from.")]
         public int InputServerPort { get; init; } = 5010;
 
         public InputConfig InputConfig { get; init; } = new();
 
-        /* If this is not null, any user participating will get that run number's participation emblem. */
+        [Description("If this is not null, any user participating will get that run number's participation emblem.")]
         public int? RunNumber = null;
 
-        /* If true, inputs are muted until explicitly unmuted, e.g. through !unmuteinputs or 'GET /start_run' */
+        [Description("If true, inputs are muted until explicitly unmuted. " +
+                     "Muting also implies that no emblems are handed out and no run statistics are recorded. " +
+                     "Inputs can be unmuted at runtime using !unmuteinputs or calling /start_run on the inputserver. " +
+                     "Inputs can be muted at runtime using !muteinputs or calling /stop_run on the inputserver. ")]
         public bool MuteInputsAtStartup = false;
     }
 }
