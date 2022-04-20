8ba3f32012ba0b51c1a243a65cbe85979f787265
felk <de.felix.koenig@gmail.com>
2022-04-20 20:56:31 +0200

    remove `wait` input

--- TPP.Core/ButtonProfiles.cs
+++ TPP.Core/ButtonProfiles.cs
@@ -16,11 +16,10 @@ namespace TPP.Core
             profile switch
             {
                 ButtonProfile.GameBoy => InputParserBuilder.FromBare()
-                    .Buttons("a", "b", "start", "select", "wait")
+                    .Buttons("a", "b", "start", "select")
                     .DPad()
                     .RemappedDPad(up: "n", down: "s", left: "w", right: "e", mapsToPrefix: "")
                     .RemappedDPad(up: "north", down: "south", left: "west", right: "east", mapsToPrefix: "")
-                    .RemappedButtons(("p", "wait"), ("xp", "wait"), ("exp", "wait"))
                     .LengthRestrictions(maxSetLength: 2, maxSequenceLength: 1)
                     .HoldEnabled(true),
                 ButtonProfile.DualGameBoy => ButtonProfile.GameBoy.ToInputParserBuilder()
