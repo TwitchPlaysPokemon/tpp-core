ce31bf138e0997bab264077b46d2260167b5372b
feselene <vaporflyb@gmail.com>
2024-10-09 20:21:22 -0400

    Add Program.cs, Startup.cs, and Website.csproj. Website works successfully in localhost. Need to set up VM or dedicated server for twitchplayspokemon.tv URL to work.

---
+++ src/Website/Startup.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;  // Add this line
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+        });
+    }
+}
