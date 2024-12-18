469066bb743021390d43c0c086ff81892bf6d810
felk <de.felix.koenig@gmail.com>
2024-12-18 20:21:37 +0100

    add and hook up new donation overlay event

--- TPP.Core/DonationHandler.cs
+++ TPP.Core/DonationHandler.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NodaTime;
 using TPP.Core.Chat;
+using TPP.Core.Overlay;
+using TPP.Core.Overlay.Events;
 using TPP.Model;
 using TPP.Persistence;
 
@@ -16,6 +20,7 @@ public class DonationHandler(
     IUserRepo userRepo,
     IBank<User> tokensBank,
     IMessageSender messageSender,
+    OverlayConnection overlayConnection,
     int donorBadgeCents)
 {
     public record NewDonation(
@@ -59,15 +64,30 @@ public class DonationHandler(
             message: donation.Message
         );
 
-        DonationTokens tokens = await GetDonationTokens(donation.Id, donation.CreatedAt, cents);
+        var recordBreaks = await GetRecordBreaks(donation.Id, donation.CreatedAt);
+        DonationTokens tokens = GetDonationTokens(cents, recordBreaks);
         if (donor != null)
         {
             await UpdateHasDonationBadge(donor);
             await GivenTokensToDonorAndNotifyThem(donor, donation.Id, tokens);
         }
         // TODO randomly distribute donation tokens
-        // TODO create event and send
-        //overlayConnection.Send()
+        await overlayConnection.Send(new NewDonationEvent
+        {
+            // We used to look up emotes using the internal Emote Service, but this small feature (emotes in donations)
+            // was the only thing remaining using the Emote Service, so it's not worth it.
+            Emotes = [],
+            RecordDonations = new Dictionary<int, List<string>>
+            {
+                [cents] = recordBreaks.Select(recordBreak => recordBreak.Name).ToList()
+            },
+            Donation = new NewDonationEvent.DonationInfo
+            {
+                Username = donation.Username,
+                Cents = cents,
+                Message = donation.Message
+            }
+        }, CancellationToken.None);
     }
 
     private async Task UpdateHasDonationBadge(User user)
@@ -83,18 +103,23 @@ public class DonationHandler(
         public int Total() => Base + Bonus;
     }
 
+    /// Assumes the donation has already been persisted.
+    private async Task<ImmutableSortedSet<DonationRecordBreakType>> GetRecordBreaks(int donationId, Instant createdAt)
+    {
+        return (await donationRepo.GetRecordDonations(createdAt))
+            .Where(kvp => kvp.Value.DonationId == donationId)
+            .Select(kvp => kvp.Key)
+            .ToImmutableSortedSet();
+    }
+
     /// Calculated a donation's reward tokens, which consists of some base tokens per cents,
     /// plus bonus tokens obtained from donation record breaks.
-    /// Assumes the donation has already been persisted.
-    private async Task<DonationTokens> GetDonationTokens(int donationId, Instant createdAt, int cents)
+    private DonationTokens GetDonationTokens(int cents, ISet<DonationRecordBreakType> recordBreaks)
     {
         const int centsPerToken = 50;
 
         int baseTokens = cents / centsPerToken;
-        int bonusTokens =
-            (await donationRepo.GetRecordDonations(createdAt))
-            .Where(kvp => kvp.Value.DonationId == donationId)
-            .Sum(kvp => kvp.Key.TokenWinning);
+        int bonusTokens = recordBreaks.Sum(recordBreakType => recordBreakType.TokenWinning);
 
         return new DonationTokens(baseTokens, bonusTokens);
     }
---
+++ TPP.Core/Overlay/Events/NewDonationEvent.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TPP.Core.Overlay.Events.Common;
+
+namespace TPP.Core.Overlay.Events;
+
+[DataContract]
+public struct NewDonationEvent : IOverlayEvent
+{
+    public string OverlayEventType => "new_donation";
+
+    [DataContract]
+    public struct DonationInfo
+    {
+        [DataMember(Name = "name")] public string Username { get; set; }
+        [DataMember(Name = "cents")] public int Cents { get; set; }
+        [DataMember(Name = "message")] public string? Message { get; set; }
+    }
+
+    [DataMember(Name = "donation")]
+    public DonationInfo Donation { get; set; }
+
+    [DataMember(Name = "emotes")]
+    public List<EmoteInfo> Emotes { get; set; }
+
+    // E.g. {200: ["1h", "2h"], 500: ["6h"]}
+    // In reality, the dictionary always ever has 1 key: the donation's cents amount. So this could theoretically just
+    // be a List<string>, but it's this way for backwards-compatibility with the old overlay.
+    [DataMember(Name = "record_donations")]
+    public Dictionary<int, List<string>> RecordDonations { get; set; }
+
+    // old core also had "potential_token_winners" and "token_winners", but those seemed unused
+}
