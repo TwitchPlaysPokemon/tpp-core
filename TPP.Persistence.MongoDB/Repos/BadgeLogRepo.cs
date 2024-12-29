5318e8f63df42f51bd4ab44a735a16a58e6e5893
felk <de.felix.koenig@gmail.com>
2024-12-29 01:59:18 +0100

    convert repos to file-scoped namespaces

--- TPP.Persistence.MongoDB/Repos/BadgeLogRepo.cs
+++ TPP.Persistence.MongoDB/Repos/BadgeLogRepo.cs
@@ -8,63 +8,62 @@ using NodaTime;
 using TPP.Model;
 using TPP.Persistence.MongoDB.Serializers;
 
-namespace TPP.Persistence.MongoDB.Repos
+namespace TPP.Persistence.MongoDB.Repos;
+
+/// <summary>
+/// MongoDB-specific extension of <see cref="IBadgeLogRepo"/> which is required because <see cref="BadgeRepo"/>
+/// needs to additionally pass <see cref="IClientSessionHandle"/>s to be able to log within a transaction.
+/// </summary>
+public interface IMongoBadgeLogRepo : IBadgeLogRepo
 {
-    /// <summary>
-    /// MongoDB-specific extension of <see cref="IBadgeLogRepo"/> which is required because <see cref="BadgeRepo"/>
-    /// needs to additionally pass <see cref="IClientSessionHandle"/>s to be able to log within a transaction.
-    /// </summary>
-    public interface IMongoBadgeLogRepo : IBadgeLogRepo
+    Task<BadgeLog> LogWithSession(
+        string badgeId, string badgeLogType, string? userId, Instant timestamp,
+        IDictionary<string, object?>? additionalData = null,
+        IClientSessionHandle? session = null);
+}
+
+public class BadgeLogRepo(IMongoDatabase database) : IMongoBadgeLogRepo, IAsyncInitRepo
+{
+    public const string CollectionName = "badgelog";
+
+    public readonly IMongoCollection<BadgeLog> Collection = database.GetCollection<BadgeLog>(CollectionName);
+
+    static BadgeLogRepo()
     {
-        Task<BadgeLog> LogWithSession(
-            string badgeId, string badgeLogType, string? userId, Instant timestamp,
-            IDictionary<string, object?>? additionalData = null,
-            IClientSessionHandle? session = null);
+        BsonClassMap.RegisterClassMap<BadgeLog>(cm =>
+        {
+            cm.MapIdProperty(b => b.Id)
+                .SetIdGenerator(StringObjectIdGenerator.Instance)
+                .SetSerializer(ObjectIdAsStringSerializer.Instance);
+            cm.MapProperty(b => b.BadgeId).SetElementName("badge")
+                .SetSerializer(ObjectIdAsStringSerializer.Instance);
+            cm.MapProperty(b => b.BadgeLogType).SetElementName("event");
+            cm.MapProperty(b => b.UserId).SetElementName("user");
+            cm.MapProperty(b => b.Timestamp).SetElementName("ts");
+            cm.MapExtraElementsProperty(b => b.AdditionalData);
+        });
     }
 
-    public class BadgeLogRepo(IMongoDatabase database) : IMongoBadgeLogRepo, IAsyncInitRepo
+    public async Task InitializeAsync()
     {
-        public const string CollectionName = "badgelog";
-
-        public readonly IMongoCollection<BadgeLog> Collection = database.GetCollection<BadgeLog>(CollectionName);
-
-        static BadgeLogRepo()
-        {
-            BsonClassMap.RegisterClassMap<BadgeLog>(cm =>
-            {
-                cm.MapIdProperty(b => b.Id)
-                    .SetIdGenerator(StringObjectIdGenerator.Instance)
-                    .SetSerializer(ObjectIdAsStringSerializer.Instance);
-                cm.MapProperty(b => b.BadgeId).SetElementName("badge")
-                    .SetSerializer(ObjectIdAsStringSerializer.Instance);
-                cm.MapProperty(b => b.BadgeLogType).SetElementName("event");
-                cm.MapProperty(b => b.UserId).SetElementName("user");
-                cm.MapProperty(b => b.Timestamp).SetElementName("ts");
-                cm.MapExtraElementsProperty(b => b.AdditionalData);
-            });
-        }
-
-        public async Task InitializeAsync()
-        {
-            await database.CreateCollectionIfNotExists(CollectionName);
-        }
-
-        public async Task<BadgeLog> LogWithSession(
-            string badgeId, string badgeLogType, string? userId, Instant timestamp,
-            IDictionary<string, object?>? additionalData = null,
-            IClientSessionHandle? session = null)
-        {
-            var item = new BadgeLog(string.Empty, badgeId, badgeLogType, userId, timestamp,
-                additionalData ?? ImmutableDictionary<string, object?>.Empty);
-            if (session != null)
-                await Collection.InsertOneAsync(session, item);
-            else
-                await Collection.InsertOneAsync(item);
-            return item;
-        }
-
-        public Task<BadgeLog> Log(string badgeId, string badgeLogType, string? userId, Instant timestamp,
-            IDictionary<string, object?>? additionalData = null) =>
-            LogWithSession(badgeId, badgeLogType, userId, timestamp, additionalData, session: null);
+        await database.CreateCollectionIfNotExists(CollectionName);
     }
+
+    public async Task<BadgeLog> LogWithSession(
+        string badgeId, string badgeLogType, string? userId, Instant timestamp,
+        IDictionary<string, object?>? additionalData = null,
+        IClientSessionHandle? session = null)
+    {
+        var item = new BadgeLog(string.Empty, badgeId, badgeLogType, userId, timestamp,
+            additionalData ?? ImmutableDictionary<string, object?>.Empty);
+        if (session != null)
+            await Collection.InsertOneAsync(session, item);
+        else
+            await Collection.InsertOneAsync(item);
+        return item;
+    }
+
+    public Task<BadgeLog> Log(string badgeId, string badgeLogType, string? userId, Instant timestamp,
+        IDictionary<string, object?>? additionalData = null) =>
+        LogWithSession(badgeId, badgeLogType, userId, timestamp, additionalData, session: null);
 }
--- TPP.Persistence.MongoDB/Repos/MessagelogRepo.cs
+++ TPP.Persistence.MongoDB/Repos/MessagelogRepo.cs
@@ -6,38 +6,37 @@ using NodaTime;
 using TPP.Model;
 using TPP.Persistence.MongoDB.Serializers;
 
-namespace TPP.Persistence.MongoDB.Repos
+namespace TPP.Persistence.MongoDB.Repos;
+
+public class MessagelogRepo(IMongoDatabase database) : IMessagelogRepo, IAsyncInitRepo
 {
-    public class MessagelogRepo(IMongoDatabase database) : IMessagelogRepo, IAsyncInitRepo
+    public const string CollectionName = "messagelog";
+
+    public readonly IMongoCollection<Messagelog> Collection = database.GetCollection<Messagelog>(CollectionName);
+
+    static MessagelogRepo()
     {
-        public const string CollectionName = "messagelog";
-
-        public readonly IMongoCollection<Messagelog> Collection = database.GetCollection<Messagelog>(CollectionName);
-
-        static MessagelogRepo()
+        BsonClassMap.RegisterClassMap<Messagelog>(cm =>
         {
-            BsonClassMap.RegisterClassMap<Messagelog>(cm =>
-            {
-                cm.MapIdProperty(i => i.Id)
-                    .SetIdGenerator(StringObjectIdGenerator.Instance)
-                    .SetSerializer(ObjectIdAsStringSerializer.Instance);
-                cm.MapProperty(i => i.UserId).SetElementName("user");
-                cm.MapProperty(i => i.Message).SetElementName("message");
-                cm.MapProperty(i => i.IrcLine).SetElementName("ircline");
-                cm.MapProperty(i => i.Timestamp).SetElementName("timestamp");
-            });
-        }
+            cm.MapIdProperty(i => i.Id)
+                .SetIdGenerator(StringObjectIdGenerator.Instance)
+                .SetSerializer(ObjectIdAsStringSerializer.Instance);
+            cm.MapProperty(i => i.UserId).SetElementName("user");
+            cm.MapProperty(i => i.Message).SetElementName("message");
+            cm.MapProperty(i => i.IrcLine).SetElementName("ircline");
+            cm.MapProperty(i => i.Timestamp).SetElementName("timestamp");
+        });
+    }
 
-        public async Task InitializeAsync()
-        {
-            await database.CreateCollectionIfNotExists(CollectionName);
-        }
+    public async Task InitializeAsync()
+    {
+        await database.CreateCollectionIfNotExists(CollectionName);
+    }
 
-        public async Task<Messagelog> LogChat(string userId, string ircLine, string message, Instant timestamp)
-        {
-            var item = new Messagelog(string.Empty, ircLine, userId, message, timestamp);
-            await Collection.InsertOneAsync(item);
-            return item;
-        }
+    public async Task<Messagelog> LogChat(string userId, string ircLine, string message, Instant timestamp)
+    {
+        var item = new Messagelog(string.Empty, ircLine, userId, message, timestamp);
+        await Collection.InsertOneAsync(item);
+        return item;
     }
 }
